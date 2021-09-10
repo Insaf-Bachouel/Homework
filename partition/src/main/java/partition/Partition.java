bfb802b9089b4c5b05bb26eafbcb3e6061309676
Insaf <insaf.bachouel@gmail.com>
2021-09-10 02:20:41 +0100

    first commit

---
+++ partition/src/main/java/partition/Partition.java
@@ -0,0 +1,40 @@
+package partition;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Partition 
+{   
+	public static <T> List<List<T>> partitionFunction(List<T> list, Integer partitionSize){
+	    int size = list.size();
+	    int partitionNumber = size / partitionSize;
+	    if (size % partitionSize != 0) {
+	    	partitionNumber++;
+	    }
+	 
+	    List<List<T>> partition = new ArrayList();
+	    for (int i = 0; i < partitionNumber; i++)
+	    {
+	        int from= i*partitionSize;
+	        int to = (i*partitionSize + partitionSize < size) ? (i*partitionSize + partitionSize) : size;
+	 
+	        partition.add(list.subList(from, to));
+	    }
+	    return partition;
+ }
+ public static void main(String[] args) {
+
+		List<Integer> testList = new ArrayList() ;
+		
+		for (int i = 1; i < 6; i++) {
+			testList.add(i);
+		}
+		System.out.println(partitionFunction(testList,2));		
+ }   
+}
---
+++ partition/src/test/java/partition/PartitionTest.java
@@ -0,0 +1,28 @@
+package partition;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+
+public class PartitionTest {
+	Partition partition ; 
+	
+	@Test
+	public  void PartitionFunctionTest() {
+		
+		List<Integer> testList = new ArrayList() ;
+	
+		for (int i = 1; i < 6; i++) {
+			testList.add(i);
+		}
+		
+		assertEquals(3, partition.partitionFunction(testList , 2).size()) ;
+		assertEquals(2, partition.partitionFunction(testList , 2).get(0).size());
+		
+	}
+	
+
+}
